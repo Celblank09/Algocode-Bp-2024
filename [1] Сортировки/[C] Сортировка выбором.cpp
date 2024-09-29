93f2318ecbd325f6fd7bdddb77a6abbb71b1a16f
Yan Vladislavovich Zaboryanskiy <yanzaboryanskiyzte@gmail.com>
2024-09-29 22:16:36 +0300

    [1] Сортировки
    Добавил решение для [C] Сортировка вставками

--- [1] Сортировки/[C] Сортировка выбором.cpp
+++ [1] Сортировки/[C] Сортировка выбором.cpp
@@ -1,3 +1,26 @@
-//
-// Created by Ян on 29.09.2024.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void selection_sort(vector<long long> &a, long long n) {
+    for (long long i = 0; i < n - 1; ++i) {
+        long long j = min_element(a.begin() + i, a.end()) - a.begin();
+        swap(a[i], a[j]);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long n;
+    cin >> n;
+    vector<long long> a(n, 0);
+    for (long long i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    selection_sort(a, n);
+    for (long long i : a) {
+        cout << i << " ";
+    }
+    return 0;
+}
