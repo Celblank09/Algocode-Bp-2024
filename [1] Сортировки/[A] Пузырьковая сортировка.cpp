20a7d6a4fbbe7be0dcd67ce7b843cfdaa887f4d1
Yan Vladislavovich Zaboryanskiy <yanzaboryanskiyzte@gmail.com>
2024-09-29 21:56:47 +0300

    [1] Сортировки
    Добавил решение для [A] Пузырьковая сортировка

--- [1] Сортировки/[A] Пузырьковая сортировка.cpp
+++ [1] Сортировки/[A] Пузырьковая сортировка.cpp
@@ -1,3 +1,29 @@
-//
-// Created by Ян on 29.09.2024.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bubble_sort(vector<long long> &a, long long n) {
+    for (long long i = n - 1; i >= 0; --i) {
+        for (long long j = 1; j <= i; ++j) {
+            if (a[j] < a[j - 1]) {
+                swap(a[j], a[j - 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long n;
+    cin >> n;
+    vector<long long> a(n, 0);
+    for (long long i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    bubble_sort(a, n);
+    for (long long i : a) {
+        cout << i << " ";
+    }
+    return 0;
+}
