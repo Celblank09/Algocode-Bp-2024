29811598a630a4020430bf763ecb540a4a788a05
Yan Vladislavovich Zaboryanskiy <yanzaboryanskiyzte@gmail.com>
2024-10-02 00:27:34 +0300

    [1] Сортировки
    Добавил решение для [K] Анаграммы

--- [1] Сортировки/[K] Анаграммы.cpp
+++ [1] Сортировки/[K] Анаграммы.cpp
@@ -1,3 +1,32 @@
-//
-// Created by Ян on 29.09.2024.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string a, b;
+    cin >> a >> b;
+    map <char, int> a1, b1;
+    for (char i : a) {
+        if (a1[i]) {
+            a1[i]++;
+        } else {
+            a1[i] = 1;
+        }
+    }
+    for (char i : b) {
+        if (b1[i]) {
+            b1[i]++;
+        } else {
+            b1[i] = 1;
+        }
+    }
+    for (char i : "qwertyuiopasdfghjklzxcvbnm1234567890") {
+        if (a1[i] != b1[i]) {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+}
