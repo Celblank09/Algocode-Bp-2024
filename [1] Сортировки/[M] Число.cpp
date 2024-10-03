4ba72aa2889b2374aa01408702d5a794ea5533c4
Yan Vladislavovich Zaboryanskiy <yanzaboryanskiyzte@gmail.com>
2024-10-03 13:58:49 +0300

    [1] Сортировки
    Добавил решение для [M] Число

--- [1] Сортировки/[M] Число.cpp
+++ [1] Сортировки/[M] Число.cpp
@@ -1,3 +1,21 @@
-//
-// Created by Ян on 29.09.2024.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp(string A, string B) {
+    return A + B > B + A;
+}
+
+int main() {
+    /*ios::sync_with_stdio(0);
+    cin.tie(0);*/
+    vector <string> a;
+    string dop;
+    while (cin >> dop) {
+        a.push_back(dop);
+    }
+    sort(a.begin(), a.end(), cmp);
+    for (string i : a) {
+        cout << i;
+    }
+}
